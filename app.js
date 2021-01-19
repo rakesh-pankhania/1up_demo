0b2c609a9491bd036ae27023a788f6db09eb6df6
Rakesh Pankhania <rakesh.d.pankhania@gmail.com>
2021-01-18 23:57:47 -0500

    Clean things up

--- app.js
+++ app.js
@@ -1,61 +1,35 @@
+// Import modules
 require('dotenv').config();
-// TODO: Remove Axios
-const axios = require('axios');
 const express = require('express');
-const { OneUpClient } = require('./modules/one_up_client.js');
+const { OneUpClient } = require('./modules/one_up_health.js');
 
+// Set global server variables
 const app = express();
 const oneUpClient = new OneUpClient(process.env.OAUTH_CLIENT_ID, process.env.OAUTH_CLIENT_SECRET);
 const port = 3000;
 
+// Generate mock database tables
+let userTokenMap = [];
+let connections = [
+  {
+    id: 4707,
+    name: 'Cerner Health Systems (demo)',
+    logo: 'https://1uphealth-assets.s3-us-west-2.amazonaws.com/systems/cerner.png'
+  }
+];
+
 app.set('view engine', 'pug')
 app.use(express.urlencoded({extended: false}));
 
-// TODO: Move users around
-const staticUserId = 1;
+
+// Dashboard route
 
 app.get('/', (req, res) => {
   res.render('index');
 })
 
-app.get('/connections', (req, res) => {
-  let userAccessToken;
 
-  axios.post('https://api.1up.health/user-management/v1/user/auth-code', {
-    app_user_id: staticUserId,
-    client_id: process.env.OAUTH_CLIENT_ID,
-    client_secret: process.env.OAUTH_CLIENT_SECRET
-  }).then((codeResponse) => {
-    let code = codeResponse.data.code
-
-    axios.post('https://api.1up.health/fhir/oauth2/token', {
-      grant_type: 'authorization_code',
-      code: code,
-      client_id: process.env.OAUTH_CLIENT_ID,
-      client_secret: process.env.OAUTH_CLIENT_SECRET
-    }).then((tokenResponse) => {
-      console.log(tokenResponse.data);
-      userAccessToken = tokenResponse.data.access_token
-
-      let connections = [
-        {
-          "id": 4707,
-          "name": "Cerner Health Systems (demo)",
-          "logo": "https://1uphealth-assets.s3-us-west-2.amazonaws.com/systems/cerner.png",
-          "url": `https://api.1up.health/connect/system/clinical/4707?client_id=${process.env.OAUTH_CLIENT_ID}&access_token=${userAccessToken}`
-        }
-      ];
-
-      console.log(connections);
-
-      res.render('connections/index', { connections });
-    });
-  });
-});
-
-/*
-  User Management
-*/
+// User Routes
 
 app.get('/users/new', (req, res) => {
   res.render('users/form');
@@ -69,6 +43,32 @@ app.get('/users', (req, res) => {
   });
 });
 
+app.get('/users/:userId', (req, res) => {
+  let userId = req.params.userId;
+  let userToken = userTokenMap[userId];
+
+  if (!userToken) {
+    oneUpClient.getToken(userId).then((userToken) => {
+      userTokenMap[userId] = userToken;
+
+      let connectionsWithUrl = connections.map(conn => {
+        return {...conn, url: oneUpClient.connectPatientUrl(conn.id, userToken)};
+      })
+
+      res.render('users/show', { userId, userToken, connections: connectionsWithUrl });
+    }).catch((response) => {
+      res.send("Error :(");
+    });
+  }
+  else {
+    let connectionsWithUrl = connections.map(conn => {
+      return {...conn, url: oneUpClient.connectPatientUrl(conn.id, userToken)};
+    })
+
+    res.render('users/show', { userId, userToken });
+  }
+})
+
 app.post('/users', (req, res) => {
   oneUpClient.createUser(req.body.applicationUserId).then((user) => {
     res.redirect('/users')
@@ -76,38 +76,6 @@ app.post('/users', (req, res) => {
     res.send("Error :(")
   });
 });
-
-app.get('/kitchen_sink', (req, res) => {
-  let code;
-  let staticUserId = 'demo'
-
-  // Get access token (POST auth-code, POST token)
-  axios.post('https://api.1up.health/user-management/v1/user/auth-code', {
-    app_user_id: staticUserId,
-    client_id: process.env.OAUTH_CLIENT_ID,
-    client_secret: process.env.OAUTH_CLIENT_SECRET
-  }).then((codeResponse) => {
-    let code = codeResponse.data.code
-
-    axios.post('https://api.1up.health/fhir/oauth2/token', {
-      grant_type: 'authorization_code',
-      code: code,
-      client_id: process.env.OAUTH_CLIENT_ID,
-      client_secret: process.env.OAUTH_CLIENT_SECRET
-    }).then((tokenResponse) => {
-      console.log(tokenResponse.data);
-      res.render('index', { title: 'Welcome', message: 'Hello World!', token: tokenResponse.data.access_token });
-    }).catch((response) => {
-      console.log("TOKEN ERROR");
-      console.log(response.data);
-      res.render('index', { title: 'Welcome', message: 'Hello World!', token: "ERROR" });
-    });
-  }).catch((response) => {
-    console.log("CODE ERROR");
-    console.log(response.data);
-    res.render('kitchen_sink', { title: 'Welcome', message: 'Hello World!', token: "ERROR" });
-  });
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ modules/one_up_health.js
@@ -0,0 +1,94 @@
+const axios = require('axios');
+const ONE_UP_ROOT_URL = 'https://api.1up.health';
+
+class OneUpClient {
+  constructor(clientId, clientSecret) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+  }
+
+  /*
+    Authentication (OAuth2)
+    https://1up.health/docs/api/auth/reference
+  */
+
+  getToken(userId) {
+    return new Promise((resolve, reject) => {
+      // Generate user authorization code
+      axios.post(`${ONE_UP_ROOT_URL}/user-management/v1/user/auth-code`, {
+        app_user_id: userId,
+        client_id: this.clientId,
+        client_secret: this.clientSecret
+      }).then((codeResponse) => {
+        // Get access token
+        axios.post(`${ONE_UP_ROOT_URL}/fhir/oauth2/token`, {
+          grant_type: 'authorization_code',
+          code: codeResponse.data.code,
+          client_id: this.clientId,
+          client_secret: this.clientSecret
+        }).then((tokenResponse) => {
+          resolve(tokenResponse.data.access_token);
+        }).catch((response) => {
+          console.error('1up client getToken() token error');
+          console.error(response);
+          reject(response);
+        });
+      }).catch((response) => {
+        console.error('1up client getToken() code error');
+        console.error(response);
+        reject(response);
+      });
+    })
+  }
+
+  /*
+    User Management
+    https://1up.health/docs/api/user-management/reference
+  */
+
+  getUsers() {
+    return new Promise((resolve, reject) => {
+      axios.get(`${ONE_UP_ROOT_URL}/user-management/v1/user`, {
+        params: {
+          client_id: this.clientId,
+          client_secret: this.clientSecret
+        }
+      }).then((response) => {
+        resolve(response.data.entry);
+      }).catch((response) => {
+        console.error('1up client getUsers() error');
+        console.error(response);
+        reject(response);
+      })
+    });
+  }
+
+  createUser(appUserId) {
+    return new Promise((resolve, reject) => {
+      axios.post(`${ONE_UP_ROOT_URL}/user-management/v1/user`, {
+        app_user_id: appUserId,
+        client_id: this.clientId,
+        client_secret: this.clientSecret
+      }).then((response) => {
+        resolve(response.data);
+      }).catch((response) => {
+        console.error('1up client createUser() error');
+        console.error(response)
+        reject(response);
+      });
+    });
+  }
+
+  /*
+    Connect (Patient)
+    https://1up.health/docs/api/connect-patient/reference
+  */
+
+  connectPatientUrl(healthSystemId, userToken) {
+    return `${ONE_UP_ROOT_URL}/connect/system/clinical/${healthSystemId}?client_id=${this.clientId}&access_token=${userToken}`;
+  }
+
+
+}
+
+module.exports.OneUpClient = OneUpClient;
